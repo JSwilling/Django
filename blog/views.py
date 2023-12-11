4a5254fce8d644efe3d01726e715aefa735115a4
JSwilling <149127287+JSwilling@users.noreply.github.com>
2023-12-11 17:26:18 -0500

    update

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 def post_list(request):
     Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': Post})
+    return render(request, 'blog/post_list.html', {'posts': post})
 
 
 def post_detail(request, pk):
